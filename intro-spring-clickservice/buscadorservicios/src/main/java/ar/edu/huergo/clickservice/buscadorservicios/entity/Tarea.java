d58db6091cd0837737b22df2eca6f810dd9b5d8c
Juanchope12 <jcadenteferreiro@alumno.huergo.edu.ar>
2025-11-17 20:08:52 -0300

    entity

---
+++ intro-spring-clickservice/buscadorservicios/src/main/java/ar/edu/huergo/clickservice/buscadorservicios/entity/Tarea.java
@@ -0,0 +1,47 @@
+package ar.edu.huergo.clickservice.buscadorservicios.entity;
+
+
+
+
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table (name = "tareas")
+@Data 
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tarea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
+    private String titulo;
+
+
+    private String descripcion;
+
+    private String creador;
+
+    private boolean completada; 
+
+
+
+
+   
+}
